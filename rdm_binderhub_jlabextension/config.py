4ba78f722d2916b95cccce69ccbaf4e48facd4e5
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2023-06-21 13:49:41 +0900

    Fix default storage provider resolution

--- rdm_binderhub_jlabextension/config.py
+++ rdm_binderhub_jlabextension/config.py
@@ -2,6 +2,7 @@ import json
 import os
 import yaml
 from datetime import datetime
+from urllib.parse import urlparse
 
 
 ENV_PREFIX = 'RDM_BINDERHUB_'
@@ -15,15 +16,31 @@ def save_env_to_home():
     with open(config_path, 'w') as f:
         f.write(json.dumps(dict([(k, v) for k, v in os.environ.items() if k.startswith('JUPYTERHUB_')])))
 
+def get_default_storage():
+    """
+    Retrieve the provider name from BINDER_REPO_URL
+    """
+    # BINDER_REPO_URL=https://(host)/(node_id)/(storage_provider)(/...)?
+    if 'BINDER_REPO_URL' not in os.environ:
+        return 'osfstorage'
+    url = os.environ['BINDER_REPO_URL']
+    path = urlparse(url).path.lstrip('/')
+    index = path.index('/')
+    storage_path = path[index + 1:]
+    if '/' not in storage_path:
+        return storage_path
+    return storage_path[:storage_path.index('/')]
+
 def get_default_config():
+    default_storage = get_default_storage()
     yyyymmdd = datetime.now().strftime('%Y%m%d')
-    to_dir = '/mnt/rdm/osfstorage/'
+    to_dir = f'/mnt/rdm/{default_storage}/'
     if 'JUPYTERHUB_USER' in os.environ and 'JUPYTERHUB_SERVER_NAME' in os.environ:
         jh_user = os.environ['JUPYTERHUB_USER']
         jh_service_name = os.environ['JUPYTERHUB_SERVER_NAME']
         to_path = os.path.join(to_dir, f'result-{jh_user}-{yyyymmdd}-{jh_service_name}/')
     else:
-        to_path = os.path.join(to_dir,f'/mnt/rdm/osfstorage/result-{yyyymmdd}/')
+        to_path = os.path.join(to_dir,f'/mnt/rdm/{default_storage}/result-{yyyymmdd}/')
     return dict(FROM_PATH='~/result', TO_PATH=to_path, TO_DIR=to_dir, USE_RSYNC=False)
 
 def get_config():
